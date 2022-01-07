e32f5593c5cdc34a416e687d80a2a1f6ae6d5d7f
Gersom SlonPay-Pc <gersomalaja@gmail.com>
2022-01-07 17:46:19 -0500

    add modal

--- src/components/Tables/TablesTableRow.js
+++ src/components/Tables/TablesTableRow.js
@@ -1,4 +1,12 @@
 import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  AlertDialogCloseButton,
+  useDisclosure,
   Avatar,
   Badge,
   Button,
@@ -17,7 +25,15 @@ function TablesTableRow(props) {
   const bgStatus = useColorModeValue("gray.400", "#1a202c");
   const colorStatus = useColorModeValue("white", "gray.400");
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = React.useRef()
+
+  const completeRow = () => {
+    onClose()
+  }
+
   return (
+  <>
     <Tr>
       <Td minWidth={{ sm: "250px" }} pl="0px">
         <Flex align="center" py=".8rem" minWidth="100%" flexWrap="nowrap">
@@ -81,7 +97,7 @@ function TablesTableRow(props) {
         </Text>
       </Td>
       <Td>
-        <Button bg="transparent">
+        <Button bg="transparent" onClick={onOpen}>
           <Text
             fontSize="md"
             color="gray.400"
@@ -93,6 +109,88 @@ function TablesTableRow(props) {
         </Button>
       </Td>
     </Tr>
+
+    {/* <ModalData /> */}
+    <AlertDialog
+      motionPreset='slideInBottom'
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+      isOpen={isOpen}
+      isCentered
+    >
+      <AlertDialogOverlay />
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <Flex direction="column">
+            <Text
+              fontSize="md"
+              color={textColor}
+              fontWeight="bold"
+              minWidth="100%"
+            >
+              {name}
+            </Text>
+            <Text fontSize="sm" color="gray.400" fontWeight="normal">
+              {email}
+            </Text>
+          </Flex>
+        </AlertDialogHeader>
+        <AlertDialogCloseButton />
+        
+        <AlertDialogBody>
+          <Tr>
+            <Td>Monto:</Td>
+            <Text margin="0 15px 0 0"></Text>
+            <Td>
+              <Text fontSize="md" color={textColor} fontWeight="bold">
+                {domain}
+              </Text>
+            </Td>           
+            <Text margin="0 15px 0 0"></Text>
+            <Td>
+              <Text fontSize="sm" color="gray.400" fontWeight="normal">
+                ({subdomain})
+              </Text>
+            </Td>           
+          </Tr>
+          <Tr>
+            <Td>Fecha:</Td>
+            <Text margin="0 15px 0 0">
+            </Text>
+            <Td>
+              <Text margin="15px 0 0 0" fontSize="md" color={textColor} fontWeight="bold" pb=".5rem">
+                {date}
+              </Text>
+            </Td>           
+          </Tr>
+          <Tr>
+            <Td>Estado:</Td>
+            <Text margin="0 15px 0 0"></Text>
+            <Td>
+              <Badge
+                bg={status ? "green.400" : bgStatus}
+                color={status ? "white" : colorStatus}
+                fontSize="16px"
+                p="3px 10px"
+                borderRadius="8px"
+              >
+                {status ? 'Completado' : 'En proceso'}
+              </Badge>
+            </Td>
+          </Tr>
+        </AlertDialogBody>
+        <AlertDialogFooter>
+          <Button ref={cancelRef} onClick={completeRow}>
+            {status ? 'Aceptar' : 'Completar'}
+          </Button>
+          <Button onClick={onClose} colorScheme='red' ml={3}>
+            Cancelar
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  </>
   );
 }
 
